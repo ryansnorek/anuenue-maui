e28721018cddacfab8417d0c5d6bb46bf7b7d03d
Ryan <snorekr@gmail.com>
2021-11-22 19:38:33 -0500

    image upload test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@
 export default function App() {
   return (
     <div className="wrapper">
-      <h1>App</h1>
+      <form>
+        <input name="pic" capture="environment" accept="images/*" type="file" alt=""/>
+      </form>
     </div>
   );
 }
