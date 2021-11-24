08022a293c638794c98ea546d3a3129d47d8c714
Ryan <snorekr@gmail.com>
2021-11-24 11:11:24 -0500

    ci fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <div className="footer">
-            <a href="https://www.facebook.com/" class="fa fa-facebook" target="_blank"></a>
-            <a href="https://www.instagram.com/anuenue_maui" class="fa fa-instagram" target="_blank"></a>
+            <a href="https://www.facebook.com/" class="fa fa-facebook" target="_blank" rel="noreferrer"></a>
+            <a href="https://www.instagram.com/anuenue_maui" class="fa fa-instagram" target="_blank" rel="noreferrer"></a>
         </div>
     )
 }
