8866b07101a82516aa918cb91165276fd6906919
Ryan <snorekr@gmail.com>
2021-11-24 13:43:12 -0500

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 export default function Login() {
+    const [values, setValues] = useState({ username:"", password:"" });
+    const handleChange = e => setValues({ ...values, [e.target.name]: e.target.value });
+    const handleSubmit = e => {
+        e.preventDefault();
+
+    }
+
     return (
-        <div>
-            <h1>Login page</h1>
+        <div className="login">
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        name="username"
+                        type="text"
+                        placeholder="username"
+                        value={values.username}
+                        onchange={handleChange}
+                    />
+                    <input 
+                        name="password"
+                        type="password"
+                        placeholder="password"
+                        value={values.password}
+                        onchange={handleChange}
+                    />
+                    <button>login</button>
+                </form>
+            </div>
         </div>
     )
 }
